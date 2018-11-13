172195b9402819c3b0cbc17516a9752f7c82b945
t-k- <clock126@126.com>
2018-11-13 17:57:25 -0500

    sorted items.

--- src/engines/plain-view.vue
+++ src/engines/plain-view.vue
@@ -1,14 +1,18 @@
 <template>
-<div>
-[ {{json.title}}, {{json.description}} {{json.time}} ]
-</div>
+<span>
+  <span v-if="json._dir">
+  ({{json.unread}} unread)
+  </span>
+
+  <span v-else>
+  [ {{json.title}}, {{json.description}} {{json.time}} ] <br/>
+  {{json.url}}
+  </span>
+</span>
 </template>
 
 <script>
 export default {
-  props: ['json'],
-  data: function () {
-    return {}
-  }
+  props: ['json']
 }
 </script>
--- src/list.vue
+++ src/list.vue
@@ -1,21 +1,36 @@
 <template>
 <div>
+  <h3 style="word-wrap: break-word"> {{ '/' + path_arr.join('/') }} </h3>
   <div style="position: relative;">
-    <h3> {{ '/' + path_arr().join('/') }} </h3>
+    <select v-model="sortby">
+      <option v-for="key in sortable_keys"
+      @click="sortBy()" v-bind:value="key">
+        Sort by "{{key}}"
+      </option>
+    </select>
+    <input type="checkbox" v-model="descending">Desc</input>
     <div style="position: absolute; right: 0; top: 0">
-    <button>Open dir</button>
-    <button>Sort by</button>
-    <button>Delete all</button>
+      <button @click="openDir()">Open dir</button>
+      <button @click="deleAll()">Delete all</button>
     </div>
   </div>
+
   <hr/>
-  <div v-for="i in items" style="position: relative;">
+
+  <div style="position: relative;">
+  <div v-for="(i, idx) in sorted_items" class="item" @click="click_item(i, idx)">
+    <span class="dir" v-if="i._dir">
+      {{i._dir}}/
+    </span>
     <component v-bind:is="bindViewComponent" v-bind:json="i"></component>
-    <div style="position: absolute; right: 0; top: 0">
-        <input v-if="!i.is_dir" type="button" value="C"/>
-        <input v-if="!i.is_dir" type="button" value="X"/>
+    <div style="display: flex" v-if="!i._dir && idx == clicked_idx">
+        <input style="flex: 1" type="button" value="Clone" @click.stop="clone(idx)"/>
+        <input style="flex: 1" type="button" value="Detail" @click.stop="detail(idx)"
+         v-if="env.detailed"/>
+        <input style="flex: 1" type="button" value="Dele" @click.stop="dele(idx)"/>
     </div>
     <hr/>
+  </div>
   </div>
 
 <pre v-show="env.debug">
@@ -27,42 +42,77 @@
 
 <script>
 export default {
+  methods: {
+    clone: function (idx) {
+      console.log("clone " + idx)
+    },
+    detail: function (idx) {
+      console.log("detail " + idx)
+    },
+    dele: function (idx) {
+      console.log("dele " + idx)
+    },
+    click_item: function (item, idx) {
+      if (item._dir)
+        this.$router.push({
+          path: this.path + '/' + item._dir
+        })
+      else if (this.clicked_idx == idx)
+        this.clicked_idx = -1;
+      else
+        this.clicked_idx = idx;
+    },
+    allow_sort: function (key) {
+      if (this.env.no_sort_keys)
+        return !this.env.no_sort_keys.includes(key);
+      else
+        return true;
+    }
+  },
+  mounted: function () {
+    if (this.env.default_sort_key) {
+      console.log(this.env.default_sort_key)
+      this.sortby = this.env.default_sort_key;
+    }
+  },
   props: ['detailed'],
   data: function () {
     return {
+      'descending': false,
+      'sortby': '',
+      'clicked_idx': -1,
       'items': [
         {
           'title': 'todo',
           'description': 'this is a todo list',
-          'time': '1234-12-32',
-          'is_dir': false
+          'url': 'https://google.com',
+          'time': 1234,
         },
         {
           'title': 'post',
-          'description': 'A post here',
-          'time': '1989-12-12',
-          'is_dir': false
+          'description': 'A post here, bla bla bla bla',
+          'url': 'https://baidu.com',
+          'time': 1989,
+        },
+        {
+          'unread': 3,
+          '_dir': 'others'
         },
         {
           'title': 'reminder',
           'description': 'some reminder here',
-          'time': '89-12-12',
-          'is_dir': true
+          'url': 'https://dodo.com',
+          'time': 3721,
         },
       ],
       env: {
-        'debug': true,
+        'no_sort_keys': ['title', 'description'],
+        'default_sort_key': 'time',
+        'debug': false,
         'refresh': 0,
+        'detailed': true,
         'view-engine': 'plain-view'
       }
-    }
-  },
-  methods: {
-    path_arr: function () {
-      const path = this.$route.path
-      const arr = path.split('/')
-      arr.splice(0, 2)
-      return arr
     }
   },
   computed: {
@@ -71,7 +121,49 @@ export default {
         return 'detail-' + this.env['view-engine']
       else
         return 'brief-' + this.env['view-engine']
+    },
+    path_arr: function () {
+      const path = this.$route.path
+      const arr = path.split('/')
+      arr.splice(0, 2)
+      return arr
+    },
+    path: function () {
+      const path = this.$route.path
+      /* strip trailing slash */
+      return path.replace(/\/$/, "")
+    },
+    sortable_keys: function () {
+      var set = {}
+      this.items.forEach((item, idx) => {
+        Object.keys(item).forEach((k) => {
+          if (this.allow_sort(k)) set[k] = true
+        })
+      })
+      return Object.keys(set)
+    },
+    sorted_items: function (items) {
+      var vm = this;
+      return this.items.sort(function (a, b) {
+        const a_val = a[vm.sortby] || '0'
+        const b_val = b[vm.sortby] || '0'
+        if (vm.descending)
+          return (a_val < b_val) ? 1 : -1
+        else
+          return (a_val < b_val) ? -1 : 1
+      })
     }
   }
 }
 </script>
+<style>
+div.item {
+  padding-top: 30px;
+}
+div.item:active {
+  background-color: grey;
+}
+span.dir {
+  color: blue;
+}
+</style>
